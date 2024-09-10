8b6bf2a5b7ed762bfdcc1954556b5499fd137912
vaibhavdhar31 <dhardubey11@gmail.com>
2024-09-11 00:48:36 +0530

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
             className='w-[366px] h-[172px] object-contain'
           />
           <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>
-            A new way to make the payments easy, reliable and secure.         
+            A new way to make reliable and prosperity.         
           </p>
         </div>
         <div className='flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10'>
@@ -39,7 +39,7 @@ const Footer = () => {
       </div>
       <div className='w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]'>
         <p className='font-poppins font-normal text-center text-[18px] leading-[27px] text-white'>
-          2023 HooBank. All Rights Reserved.
+          #2007-2024 @OnePoint. All Rights Reserved.
         </p>  
         <div className='flex flex-row md:mt-0 mt-6'>
           {socialMedia.map((social, index) => (
