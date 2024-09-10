2770b2346529e3747073582e41f7cd801ad50a89
vaibhavdhar31 <dhardubey11@gmail.com>
2024-09-11 00:25:46 +0530

    changes in CTA,jsx file

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -6,64 +6,62 @@ import gmail from "../assets/gmail.png";
 import telegram from "../assets/telegram.png";
 import insta from "../assets/instagram.png";
 
-
 const CTA = () => {
   return (
     <section
       className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow p-10 sm:p-12`}
     >
       <div className="flex-1 flex flex-col">
-        <h2 className={styles.heading2}>Let's try our service now!</h2>
+      <h2 className={`${styles.heading2} text-2xl sm:text-4xl`}>Let's try our service now!</h2>
 
-        <div  className="flex flex-row items-center gap-8 mt-4">
+        {/* Icons for desktop and mobile view */}
+        <div className="flex flex-wrap items-center gap-5 mt-4">
+          <div className="w-[40px] h-[40px] sm:w-[60px] sm:h-[60px] rounded-full flex justify-center items-center bg-dimBlue">
+            <a
+              href="https://api.whatsapp.com/send/?phone=%2B916388453213&text=Hello+Vaibhav"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={whatsapp} alt="Whatsapp" className="w-full h-full object-contain" />
+            </a>
+          </div>
 
-        <div class="w-[64px] h-[64px] rounded-full flex justify-center items-center bg-dimBlue" >
-          <a
-            href="https://api.whatsapp.com/send/?phone=%2B919315569233&text=Hello+Anoop"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={whatsapp} alt="Whatsapp" />
-          </a>
-        </div>
+          <div className="w-[40px] h-[40px] sm:w-[60px] sm:h-[60px] rounded-full flex justify-center items-center bg-dimBlue">
+            <a
+              href="mailto:pramodtejrapiku01@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={gmail} alt="gmail" className="w-full h-full object-contain" />
+            </a>
+          </div>
 
-        <div class="w-[64px] h-[64px] rounded-full flex justify-center items-center bg-dimBlue" >
-          <a
-            href="mailto:onepointms1@gmail.com"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={gmail} alt="gmail" />
-          </a>
-        </div>
+          <div className="w-[40px] h-[40px] sm:w-[60px] sm:h-[60px] rounded-full flex justify-center items-center bg-dimBlue">
+            <a
+              href="https://t.me/dhardubey11"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={telegram} alt="telegram" className="w-full h-full object-contain" />
+            </a>
+          </div>
 
-        <div class="w-[64px] h-[64px] rounded-full flex justify-center items-center bg-dimBlue" >
-          <a
-            href="https://t.me/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={telegram} alt="telegram" />
-          </a>
-        </div>
-
-        <div class="w-[64px] h-[64px] rounded-full flex justify-center items-center bg-dimBlue" >
-          <a
-            href="https://www.instagram.com/vaibhavdhard"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={insta} alt="Instagram" />
-          </a>
-        </div>
-        
+          <div className="w-[40px] h-[40px] sm:w-[60px] sm:h-[60px] rounded-full flex justify-center items-center bg-dimBlue">
+            <a
+              href="https://www.instagram.com/vaibhavdhard"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={insta} alt="Instagram" className="w-full h-full object-contain" />
+            </a>
+          </div>
         </div>
 
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
           Write some content 
         </p>
       </div>
-      <div className={`${styles.flexCenter} sm:ml-10 ml-0`}>
+      <div className={`${styles.flexCenter} sm:ml-10 ml-0 mt-5 sm:mt-0`}>
         <Button />
       </div>
     </section>
