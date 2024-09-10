9e3821925aa826d6f38cfa90cf03c652997a6c68
vaibhavdhar31 <dhardubey11@gmail.com>
2024-09-11 00:44:47 +0530

    clients overlapping

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -5,7 +5,7 @@ import styles from '../style'
 const Clients = () => {
   return (
     <section className={`${styles.flexCenter} my-4`}>
-      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full gap-8`}>
         {clients.map((client) => (
           <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
             <img
